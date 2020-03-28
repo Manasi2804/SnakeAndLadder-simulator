768900fc86f1f51e5e646e8638e761b0442824aa
Manasi2804 <shahmanasi281996@gmail.com>
2020-03-28 08:43:44 +0530

    Added:Option check for snake,ladder and no play

--- snakeAndladder.sh
+++ snakeAndladder.sh
@@ -1,6 +1,26 @@
 #!/bin/bash -x
 
 FirstPosition=0
-echo "Welcome to the snake and ladder simulator"
-diceNumber=$(( RANDOM%6+1 ))
-echo $diceNumber
+CountDicePlayed=0
+numberofPlayer=2
+
+if [[ $numberofPlayer -eq 2 ]]
+	then
+		diceNumber=$(( RANDOM%6+1 ))
+		echo "First player position:" $FirstPosition
+		(( countDicePlayed++ ))
+		ch=$(( RANDOM%3+1 ))
+		case $ch in
+		1)  #no play
+			FirstPosition=$FirstPosition  ;;
+
+		2)  #ladder
+			FirstPosition=$(( $FirstPosition+$diceNumber ))  ;;
+
+		3)  #snake
+			FirstPosition=$(( $FirstPosition-$diceNumber ))  ;;
+		esac
+		if [[ $FirstPosition -gt 100 ]]
+		then
+			FirstPosition=$(( $FirstPosition-$diceNumber ))
+		fi
